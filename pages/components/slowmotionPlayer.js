2b291a451506d98c90709138802bde3ddd96a34b
thomas_felipe@live.com <thomas_felipe@live.com>
2022-02-13 15:51:05 -0300

    addition of ultra slow motion view

--- pages/components/slowmotionPlayer.js
+++ pages/components/slowmotionPlayer.js
@@ -1,13 +1,24 @@
-import { React } from 'react';
+import { React, useState } from 'react';
+import Image from 'next/image';
 
 import style from '../../styles/Slowmotionplayer.module.css';
 import VideoCarousel from './videocarousel';
+import ViewFinder from '../content/images/VIEWFINDER.png'
 
 export default function SlowmotionPlayer() {
+    const [isSlowMotionOn, setIsSlowMotionOn] = useState(false);
+
+    const handleSlowMotion = state => {
+        setIsSlowMotionOn(state);
+    }
 
     return (
          <div className={style['background']}>
-             <VideoCarousel />
+            <Image src={ViewFinder} width="720" height ="480" alt=""/>
+            <VideoCarousel slowMotion={isSlowMotionOn} />
+            <button onMouseOver={() => handleSlowMotion(true)} onMouseLeave={() =>handleSlowMotion(false)}>
+                <a>Activate Ultra Slow Motion</a>
+            </button>
          </div>
     )
-}
+} 
--- pages/components/videocarousel.js
+++ pages/components/videocarousel.js
@@ -1,41 +1,26 @@
-import { React } from 'react';
+import { React, useEffect } from 'react';
+import style from '../../styles/VideoCarousel.module.css';
 
-import style from '../../styles/Slowmotionplayer.module.css';
+export default function VideoCarousel(props) {
 
-// import Video1 from '../content/videos/video-1.mp4';
-// import Video2 from '../content/videos/video-2.mp4';
-// import Video3 from '../content/videos/video-3.mp4';
-// import Video4 from '../content/videos/video-4.mp4';
-// import Video5 from '../content/videos/video-5.mp4';
-// import Video6 from '../content/videos/video-6.mp4';
-// import Video7 from '../content/videos/video-7.mp4';
-// import Video8 from '../content/videos/video-8.mp4';
-// import Video9 from '../content/videos/video-9.mp4';
-// import Video10 from '../content/videos/video-10.mp4';
-// import Video11 from '../content/videos/video-11.mp4';
-// import Video12 from '../content/videos/video-12.mp4';
-// import Video13 from '../content/videos/video-13.mp4';
-// import Video14 from '../content/videos/video-14.mp4';
-// import Video15 from '../content/videos/video-15.mp4';
-// import Video16 from '../content/videos/video-16.mp4';
-// import Video17 from '../content/videos/video-17.mp4';
-// import Video18 from '../content/videos/video-18.mp4';
-// import Video19 from '../content/videos/video-19.mp4';
-// import Video20 from '../content/videos/video-20.mp4';
-// import Video21 from '../content/videos/video-21.mp4';
-// import Video22 from '../content/videos/video-22.mp4';
-// import Video23 from '../content/videos/video-23.mp4';
-// import Video24 from '../content/videos/video-24.mp4';
-// import Video25 from '../content/videos/video-25.mp4';
+    useEffect(() => {
+        handlePlaybackRate(props.slowMotion);
+    }, [props.slowMotion])
 
-export default function VideoCarousel() {
+    const handlePlaybackRate = state => {
+        const vid = document.getElementById('videoplayer');
+        if(state === true) {
+            vid.playbackRate = 1;
+        } else {
+            vid.playbackRate = 16;
+        }
+    }
 
     return (
          <div className={style['background']}>
-             a
-            <video width="320" height="240" controls >
-                <source  src="/pages/videos/teste.webm" />
-            </video>
+                <video id="videoplayer" width="720" controls={false} height="480" autoPlay muted loop style={{position:'relative', top:'-500px', left:'0', zIndex: '-1'}} >
+                    <source  src="/videos/video.mp4" type="video/mp4"/>
+                </video>
          </div>
     )
 }
